7272084072f9f1d839729e52e7785ef5c4a14e17
Mark Terrel <mark@unbounded.systems>
2022-06-10 14:59:44 -0600

    feat: Add front-end UI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { TodoitemsModule } from './todoitems/todoitems.module';
 
@@ -7,7 +9,13 @@ const databaseUrl =
   process.env.DATABASE_URL || 'mongodb://localhost:27017/test';
 
 @Module({
-  imports: [MongooseModule.forRoot(databaseUrl), TodoitemsModule],
+  imports: [
+    MongooseModule.forRoot(databaseUrl),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    TodoitemsModule,
+  ],
   controllers: [],
   providers: [],
 })
