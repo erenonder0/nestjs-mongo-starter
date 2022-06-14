1088ed75244143ddcc5592e51cef8e656febaffd
Mark Terrel <mark@unbounded.systems>
2022-06-14 08:57:24 -0600

    fix: Use correct _id prop for items in client

--- public/js/client.js
+++ public/js/client.js
@@ -23,14 +23,14 @@ const initTodo = () => {
     if (!done) return title;
     return `<s>${title}</s>`;
   };
-  const renderItem = ({ id, title, done }) => `
+  const renderItem = ({ _id, title, done }) => `
         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
             <input
                 class="form-check-input me-2"
                 type="checkbox"
                 value=""
                 ${done ? 'checked' : ''}
-                onchange="handleTodoChange(event, '${id}')"
+                onchange="handleTodoChange(event, '${_id}')"
             />
             <div style="flex-grow: 1;">
                 ${renderTitle({ title, done })}
